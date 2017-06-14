b758bbf8e624313c8a855c1087b8532143f05f9b
niksher <niksher18@gmail.com>
2017-06-14 12:56:18 +0300

    keyboard

--- src/BotCommands/StartCommand.php
+++ src/BotCommands/StartCommand.php
@@ -3,7 +3,7 @@
 namespace Longman\TelegramBot\Commands\SystemCommands;
 
 use Longman\TelegramBot\Commands\SystemCommand;
-use Longman\TelegramBot\Entities\InlineKeyboard;
+use Longman\TelegramBot\Entities\Keyboard;
 use Longman\TelegramBot\Request;
 
 class StartCommand extends SystemCommand
@@ -19,13 +19,9 @@ class StartCommand extends SystemCommand
 
         $chat_id = $message->getChat()->getId();  
         
-        $switch_element = mt_rand(0, 9) < 5 ? 'true' : 'false';
-        $inline_keyboard = new InlineKeyboard([
-            ['text' => 'inline', 'switch_inline_query' => $switch_element],
-            ['text' => 'inline current chat', 'switch_inline_query_current_chat' => $switch_element],
-        ], [
-            ['text' => 'callback', 'callback_data' => 'identifier'],
-            ['text' => 'open url', 'url' => 'https://github.com/php-telegram-bot/core'],
+        $inline_keyboard = new Keyboard([
+            ['text' => 'Кот картинкой', 'callback_data' => 'cat'],
+            ['text' => 'Видео кот', 'callback_data' => 'catgif'],
         ]);
         
 
